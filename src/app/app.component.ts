5fe04713bd4a9bc5a167968c396bfbe6acf1a984
lorenzomariabruni <lorenzomaria.bruni@gmail.com>
2025-12-24 12:30:29 +0100

    Fix missing SCSS files and update components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  template: `
-    <router-outlet></router-outlet>
-  `
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Quiz Multiplayer';
+}
